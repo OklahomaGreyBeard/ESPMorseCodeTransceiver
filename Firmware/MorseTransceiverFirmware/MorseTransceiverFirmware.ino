49a7bb2f20b0437b50b0b66c3766124068e20489
OklahomaGreyBeard <oklahomagreybeard@gmail.com>
2018-01-23 14:53:19 -0600

    Update MorseTransceiverFirmware.ino

--- Firmware/MorseTransceiverFirmware/MorseTransceiverFirmware.ino
+++ Firmware/MorseTransceiverFirmware/MorseTransceiverFirmware.ino
@@ -41,7 +41,7 @@
 ESP8266WiFiMulti WiFiMulti;
 
 // Instantiate server objects
-ESP8266WebServer server = ESP8266WebServer(80);
+ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 /* CONSTANTS */
